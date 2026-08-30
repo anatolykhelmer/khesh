3bdfd5fb91680b3ba672898eee2504a6c84c27b2
Anatoly Khelmer <anatoly.khelmer@gmail.com>
2026-08-30 18:38:21 +0300

    refactor: split the stylesheet into a token layer, base, and components

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from "./app/App";
 import { LedgerProvider } from "./app/LedgerProvider";
 import "./app/i18n";
-import "./app/styles.css";
+import "./app/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
